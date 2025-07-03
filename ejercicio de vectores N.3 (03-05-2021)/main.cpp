436958dd905009f140e2ee817f6983949aec2c8f
alerojmor <aleromo1988@hotmail.com>
2025-07-03 12:12:57 -0300

    Update main.cpp

--- ejercicio de vectores N.3 (03-05-2021)/main.cpp
+++ ejercicio de vectores N.3 (03-05-2021)/main.cpp
@@ -1,6 +1,6 @@
 /*3. Ingresar un valor entero N (< 20). A continuación ingresar un conjunto VEC de N componentes.
 A partir de este conjunto generar otro FACT en el que cada elemento sea el factorial del elemento
-homólogo de VEC. Finalmente imprimir ambos vectores a razón de un valor de cada uno por renglón #include <iostream>*/
+homólogo de VEC. Finalmente imprimir ambos vectores a razón de un valor de cada uno por renglón*/
 
 #include<iostream>
 using namespace std;
